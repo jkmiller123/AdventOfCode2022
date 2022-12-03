c38b24b376f5e4267b85afb8dad923298841de60
Kevin Miller <jkmiller123@gmail.com>
2022-12-02 19:09:54 -0500

    Day 02  First round complete round 2 complete.

--- Day_02_RockPaperScissors/Day02.py
+++ Day_02_RockPaperScissors/Day02.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 testing = False  # used for debugging messages and to pick the file
-stage_1 = True  # used for more complex coding changes
+stage_1 = False  # used for more complex coding changes
 file2read = 'Day02_sample.txt' if testing else 'Day02_01.txt'
 
 # Translate their throws to values
@@ -28,18 +28,53 @@ wl_dict = {
     "B Z": 6,  # Paper vs Scissors "loss" ,
     "C X": 6  # Scissors vs Rock  win
 }
+wl2_dict = {
+    "A X": 0,  # Rock vs Scissors     "loss" ,
+    "B X": 0,  # Paper vs Rock    "loss",
+    "C X": 0,  # Scissors vs Paper  loss
 
+    "C Y": 3,  # Scissors vs Scissors draw
+    "B Y": 3,  # Paper vs Paper  draw
+    "A Y": 3,  # Rock vs Rock    draw ,
 
+    "C Z": 6,  # Scissors vs rock win
+    "A Z": 6,  # Rock vs paper "win" ,
+    "B Z": 6   # Paper vs scissors "win" ,
+
+}
+wl3_dict = {
+    "A X": 3,  # Rock vs Scissors     "loss" ,
+    "B X": 1,  # Paper vs Rock    "loss",
+    "C X": 2,  # Scissors vs Paper  loss
+
+    "C Y": 3,  # Scissors vs Scissors draw
+    "B Y": 2,  # Paper vs Paper  draw
+    "A Y": 1,  # Rock vs Rock    draw ,
+
+    "C Z": 1,  # Scissors vs rock win
+    "A Z": 2,  # Rock vs paper "win" ,
+    "B Z": 3   # Paper vs scissors "win" ,
+
+}
 def read_file(a_file):
     score = 0
+    score2 = 0
     running_total = 0
+    running2_total = 0
     with open(a_file) as f:
         for line_in in f.readlines():
             throws = line_in.split()
             score = wl_dict[line_in.strip()] + rps_dict[throws[1]]
+            score2 = wl2_dict[line_in.strip()] + wl3_dict[line_in.strip()]
             running_total += score
+            running2_total += score2
 
-            print(f"{line_in.strip()} --> {wl_dict[line_in.strip()]} =  {score}  --> {running_total}")
+            if stage_1:
+                print(f"{line_in.strip()} --> {wl_dict[line_in.strip()]} =  {score}  --> {running_total}")
+            else:
+                print(f"{line_in.strip()} --> {wl2_dict[line_in.strip()]} =  {score2}  --> {running2_total}")
+
         print(f"Final score: {running_total}")
+        print(f"Final score2: {running2_total}")
 
 read_file(file2read)
